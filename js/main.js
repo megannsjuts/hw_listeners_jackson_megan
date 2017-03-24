0f0711daf5c6ca2b38c6a633d95ae7e73bf32af0
Megan Jackson <meganjackson@ufl.edu>
2017-03-24 10:49:30 -0700

    fix error with js code by defining the i variable

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var mainHeader = document.getElementById('mainHeader');
 
+var i = 1;
+
 mainHeader.addEventListener('click', function () {
 	var p = document.createElement('p');
 	p.innerHTML = 'This is click number ' + i++;
