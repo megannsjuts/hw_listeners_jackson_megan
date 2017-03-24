27f10887cafbb67282ad1235878bb43b14792321
Megan Jackson <meganjackson@ufl.edu>
2017-03-24 13:43:52 -0700

    add console logs to js file.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 document.addEventListener('submit', function () {
-
+	text.preventDefault();
+	console.log('The firstName is: ')
+	console.log('The lastName is: ')
+	console.log('The email is: ')
+	console.log('The message is: ')
 });
