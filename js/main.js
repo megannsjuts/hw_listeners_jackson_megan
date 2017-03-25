a6eb1f06d679d15677e2f32a224830d69976bb6b
Megan Jackson <meganjackson@ufl.edu>
2017-03-24 17:23:01 -0700

    add the third JavaScript event listener in the js file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// Event Listener #1: When you click on the header once it adds a paragraph element below the h1
+// Event Listener #1: When you doubleclick on the header once it adds a paragraph element below the h1
 
 var mainHeader = document.getElementById('mainHeader');
 
-mainHeader.addEventListener('click', function () {
+mainHeader.addEventListener('dblclick', function () {
 	var p = document.createElement('p');
 	p.innerHTML = 'And his name is Ese´!';
 	document.getElementById('mainHeader').appendChild(p);
@@ -16,4 +16,12 @@ mainHeader.addEventListener('mouseover', function (event) {
 	event.target.style.color = '#CFDBC5';
 });
 
-// Event Listener #3: When you...
+// Event Listener #3: When you click on the image it adds a paragraph element in the container
+
+var theImage = document.images[0];
+
+theImage.addEventListener('click', function () {
+	var p = document.createElement('p');
+	p.innerHTML = '<p>This is a picture of a cactus!</p>';
+	document.getElementsByClassName('container')[0].appendChild(p);
+});
